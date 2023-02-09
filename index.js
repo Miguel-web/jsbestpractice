5240421d2c1030f2e1a4427cc31fa7c8e220dd69
Miguel-web <miguelgilf@gmail.com>
2023-02-09 15:51:15 +0000

    uso de insertar variable en un string

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+// js1
 
 function multi(param1,param2) {
     return param1*param2;
 }
 console.log(multi(7,53812212));
+
+// js2
+
+// Your code goes here:
+const renderPerson = (name, date, eyeColor, age, sex) => { 
+
+    
+     
+    return `${name} is a ${age} years old ${sex} born in ${date} with ${eyeColor} eyes`; 
+
+} 
+
+
+  ///*** Do not edit below this line ****/
+  console.log(renderPerson('Bob', '05/22/1983', 'green', 23, 'male'));
