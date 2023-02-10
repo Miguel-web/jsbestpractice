f4c5f2260e8f24e1902c76f6e887be43aa113ccf
Miguel-web <miguelgilf@gmail.com>
2023-02-10 18:39:45 +0000

    array clasificado alfabeticamente

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// js1
+// js1   ******************************************************************
 
 function multi(param1,param2) {
     return param1*param2;
 }
 console.log(multi(7,53812212));
 
-// js2
+// js2   ******************************************************************
 
 // Your code goes here:
 const renderPerson = (name, date, eyeColor, age, sex) => { 
@@ -18,3 +18,18 @@ const renderPerson = (name, date, eyeColor, age, sex) => {
 
   ///*** Do not edit below this line ****/
   console.log(renderPerson('Bob', '05/22/1983', 'green', 23, 'male'));
+
+  // js3  ******************************************************************
+
+  var names = ['John', 'Kenny', 'Tom', "Bob", 'Dilan'];
+
+function sortNames(arr){
+    return arr.sort()
+}
+
+// otra forma de hacerlo , me gusta mas esta
+/* function sortNames(){
+    return name.sort()
+}*/
+
+console.log(sortNames(names));
