65a7c32518f60dd59196ce7d61db7e68c15c3488
Miguel-web <miguelgilf@gmail.com>
2023-02-27 19:22:08 +0000

    confirmar commit anterior

--- index.jsx
+++ index.jsx
@@ -49,3 +49,63 @@ const animalsInHTML = animals.map((singleAnimal) => {
 ReactDOM.render(<ul>{animalsInHTML}</ul>, document.querySelector("#myDiv"));
 
 //  React js 3  ******************************************************************
+
+// QUÉ: Esta función devuelve un string que será renderizado 
+const PrintHello = () => {
+    return <h1>Hello World</h1>;
+}
+               //qué           //dónde
+ReactDOM.render(PrintHello(), myDiv);
+
+//  React js 4  ******************************************************************
+
+import React from "react"; //Main React.js library
+import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
+
+const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
+
+/**
+ * 1) Create the mapping function and use it to generate a new array of
+ * planets in html called planetsInHTML
+ */
+const planetsInHTML = planets.map((item, i) => {
+    return (
+        (
+            <li key={i} className="list-group-item">
+                {item}
+            </li>
+        ),
+        (
+            <li key={i} className="list-group-item">
+                {item}
+            </li>
+        ),
+        (
+            <li key={i} className="list-group-item">
+                {item}
+            </li>
+        ),
+        (
+            <li key={i} className="list-group-item">
+                {item}
+            </li>
+        ),
+        (
+            <li key={i} className="list-group-item">
+                {item}
+            </li>
+        ),
+        (
+            <li key={i} className="list-group-item">
+                {item}
+            </li>
+        )
+    );
+});
+
+// 2) add the array planetsInHTML inside the innerHTML of this ul
+const content = <ul className="list-group m-5">{planetsInHTML}</ul>;
+
+ReactDOM.render(content, document.querySelector("#myDiv"));
+
+//  React js 5  ******************************************************************
